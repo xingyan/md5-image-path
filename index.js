95ccda69fde40cbd9f0e2734a05c14553aa892a4
yanx <yanx@oupeng.com>
2015-06-11 11:04:21 +0800

    init project

---
+++ index.js
@@ -0,0 +1,37 @@
+var util = require('g-file');
+var md5 = require('blueimp-md5').md5;
+
+module.exports = function(opts) {
+  if(!opts.files) throw new Error('Need path in module md5-image-path');
+  var filesList = [];
+  if((typeof opts.files).toLowerCase() == 'string') {
+    var path = getMd5Path(opts.files);
+    if(path) {
+      filesList.push({
+        file: opts.files,
+        md5: path
+      });
+    }
+  } else if(Array.isArray(opts.files)) {
+    opts.files.forEach(function(item) {
+      var path = getMd5Path(item);
+      if(path) {
+        filesList.push({
+          file: item,
+          md5: path
+        });
+      }
+    });
+  }
+  return filesList;
+}
+
+/**
+ * @desc 获得路径的md5
+ */
+function getMd5Path(str) {
+  if(util.isFile(str)) {
+    return md5(str);
+  }
+  return null;
+}
